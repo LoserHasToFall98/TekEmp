a7a71b57934a9494703d48e88094f1a8676086ab
LoserHasToFall98 <Pradyumn.Upadhyay@techconsulting.tech>
2026-04-29 09:26:09 -0400

    Added Colors, Style, SampleComposable

---
+++ features/theme/src/main/java/com/tc/theme/Colors.kt
@@ -0,0 +1,6 @@
+package com.tc.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val appBackgroundColor = Color.Green
---
+++ features/theme/src/main/java/com/tc/theme/Styles.kt
@@ -0,0 +1,5 @@
+package com.tc.theme
+
+import androidx.compose.ui.text.TextStyle
+
+val mediumTextStyle = TextStyle()
