d43d93ecf8e5395c63c9d15e05b7e956787f4a8b
Kibrom Yemaneab <kibr341@gmail.com>
2026-04-29 12:47:57 -0400

    my feature commit

--- features/theme/src/main/java/com/tc/theme/Colors.kt
+++ features/theme/src/main/java/com/tc/theme/Colors.kt
@@ -1,6 +1,29 @@
 package com.tc.theme
 
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 
-val appBackgroundColor = Color.Green
+val appBackgroundColor = Color.Blue
+//Dashboard Screen
+
+private val dashboardColors =Color(0xFFfcfbf7)
+private val  ButtonColor =Color(0xFF24332a)
+private val TextColor = Color(0xFFfbfaf6)
+private val profileColor= Color(0xFF291c13)
+private val notificationColor= Color(0xFF291c13)
+private val ButtonDueSoonColor= Color(0xFFeac3b0)
+private val clockColor= Color (0xFF97a299)
+
+//StartUpDateScreen
+private val buttonColor =Color(0xFF008000)
+private val backgroundColor= Color(0xFFD3D3D3)
+private val IconColors= Color(0xFF788FA4)
+
+//Tasks
+private val cautionIconColor= Color(0xFFFF5722)
+private val cautionIconColorS= Color(0xFF6ba1f5)
+private val taskButtonsColor =Color(0xFFebeff5)
+private val taskShowCase =Color(0xFFdbe9fe)
+private val taskIt = Color(0xFF9639e9)
+private val taskGrievance = Color(0xFFdeb756)
--- features/theme/src/main/java/com/tc/theme/SampleComposable.kt
+++ features/theme/src/main/java/com/tc/theme/SampleComposable.kt
@@ -2,12 +2,16 @@ package com.tc.theme
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SampleComposable(modifier : Modifier = Modifier){
 
-    Column(modifier.fillMaxSize()) { }
+    Column(modifier.fillMaxSize()) {
+        Text("name")
+    }
 
 }
