d93f22efe1eb1235e85b865846e7095557736955
LoserHasToFall98 <Pradyumn.Upadhyay@techconsulting.tech>
2026-04-29 08:56:06 -0400

    initialised project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TekEmp"
+include(":app")
+include(":features:dashboard")
+include(":features:tasks")
+include(":features:support")
+include(":features:profile")
+include(":features:forms")
+include(":features:auth")
+include(":app:nav")
