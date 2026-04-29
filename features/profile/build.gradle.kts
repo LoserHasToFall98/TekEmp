9998fd9038a3fe4209569c3cbaba3f92744ec92c
LoserHasToFall98 <Pradyumn.Upadhyay@techconsulting.tech>
2026-04-29 09:45:36 -0400

    Migrated to kotlin 2.3.21

--- features/profile/build.gradle.kts
+++ features/profile/build.gradle.kts
@@ -29,15 +29,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
+}
+kotlin {
+    jvmToolchain(11)
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(project(":features:theme"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
